91863e2c50d56ad9c3b04bdfa5fac9dbf7f4d61c
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2023-04-07 17:25:52 -0300

    add: import useState e criado a pasta types para armazenar os padrões de valores do item

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { useState } from 'react'
 import Header from "./components/header"
+import { Item } from './types/item'
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    // Exemplo de item:
+    // { id: 1, name: "Ler", done: false }
+  ])
+
   return (
     <Header />
   )
